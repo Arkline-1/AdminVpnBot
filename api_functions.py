2423317cfe11f59c2b34124c5d4d0e2ead7cd00c
Arkline <mikegamesaccount@mail.ru>
2026-06-13 01:48:05 +0300

    feat: function to get user-list (without logging)

--- api_functions.py
+++ api_functions.py
@@ -1,6 +1,9 @@
 # Импорт необходимых библиотек
 
+from datetime import datetime
+#import logging
 import requests
+from zoneinfo import ZoneInfo
 
 
 # Функция для создания сессии
@@ -32,3 +35,44 @@ def get_session(base_url: str, username: str, password: str) -> requests.Session
         pass
 
     return None
+
+
+# Получаем список пользователей
+
+def get_user_list(base_url: str, session: requests.Session):
+    result = []
+    url = f"{base_url}/panel/api/clients/list"
+
+
+    try:
+        response = session.get(url)
+
+        if response:
+            answer = response.json()
+
+            for element in answer["obj"]:
+                id = element["id"]
+                email = element["email"]
+                comment = element["comment"]
+                if element["traffic"]["lastOnline"]:
+                    last_online_int = datetime.fromtimestamp(element["traffic"]["lastOnline"] / 1000.0, tz=ZoneInfo("Europe/Moscow"))
+                    last_online = last_online_int.strftime("%d.%m.%Y %H:%M:%S")
+                else:
+                    last_online = "Никогда"
+                
+                string = f"({id}) [{email}] -- '{comment}', последний раз в сети: {last_online}"
+                result.append(string)
+            
+            return result
+        else:
+            # Тут сделать логи и записать в них response.status_code
+            pass
+
+    except Exception:
+        # Тут сделать логи и записать в них Exception
+        pass
+
+    return None
+
+
+# ...
