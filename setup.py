e4834e711baa29d595de9569bada968ea931cea4
Arkline <mikegamesaccount@mail.ru>
2026-06-14 12:52:08 +0300

    refactor: Safe startup despite .env errors and creation of the logs folder

--- setup.py
+++ setup.py
@@ -1,32 +1,38 @@
 # Импорт необходимых библиотек
 
-import os
 from dotenv import load_dotenv
+# import logging
+import os
+import sys
 
 
 # Получение всех нужных токенов лежащих в .env
 
 load_dotenv()
 
-API = os.getenv("API")
-TG_TOKEN = os.getenv("TG_TOKEN")
-TG_CHAT_ID = int(os.getenv("TG_CHAT_ID"))
+try:
+    API = os.getenv("API")
+    TG_TOKEN = os.getenv("TG_TOKEN")
+    TG_CHAT_ID = int(os.getenv("TG_CHAT_ID"))
 
-BASE_URL = os.getenv("BASE_URL")
-USERNAME = os.getenv("USERNAME")
-PASSWORD = os.getenv("PASSWORD")
+    BASE_URL = os.getenv("BASE_URL")
+    USERNAME = os.getenv("USERNAME")
+    PASSWORD = os.getenv("PASSWORD")
 
-LIMIT_IP = os.getenv("LIMIT_IP")
-TOTAL_GB = os.getenv("TOTAL_GB")
-INBOUNDS_IDS = os.getenv("INBOUNDS_IDS").split(",")
+    LIMIT_IP = int(os.getenv("LIMIT_IP"))
+    TOTAL_GB = int(os.getenv("TOTAL_GB"))
+    INBOUNDS_IDS = os.getenv("INBOUNDS_IDS").split(",")
+except Exception as e:
+    print(f"Ошибка при инициализации переменных: {e}")
+    sys.exit()
 
 
 # Создание необходимой файловой структуры (логи)
 
-try:
-    os.mkdir("logs/")
-except Exception:
+os.makedirs("logs/", exist_ok=True)
+
+
+# Заготовка для функции для настройки логирования
+
+def init_logging():
     pass
-
-
-# ДОБАВИТЬ: Настройка логирования
